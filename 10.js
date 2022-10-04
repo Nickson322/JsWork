4b25784ee9d232354fbef25e69f6ed2175012766
Nickson322 <77890953+Nickson322@users.noreply.github.com>
2022-10-04 11:10:50 +0300

    9, 10

---
+++ 10.js
@@ -0,0 +1,14 @@
+/** ЗАДАЧА 10 - Let
+ *
+ * Измените код так, чтобы ошибка исчезла
+ */
+
+const FavoriteAnimal = 'Monkey'
+
+console.log(FavoriteAnimal)
+    // 'Monkey'
+
+const myFavoriteAnimal = 'Cat'
+
+console.log(myFavoriteAnimal)
+    // 'Cat'
