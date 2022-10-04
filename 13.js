5fe678d39f368d157f3007d774992d325a8f2504
Nickson322 <77890953+Nickson322@users.noreply.github.com>
2022-10-04 11:49:49 +0300

    Add files via upload

---
+++ 13.js
@@ -0,0 +1,18 @@
+/** ЗАДАЧА 13 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+let human = {
+    name: "Nikita",
+    surname : "Patrakeev",
+    favouriteNumber : 37
+}
+
+console.log("My name is " + human.name + " " + human.surname + " and my favourite number is " + human.favouriteNumber)
