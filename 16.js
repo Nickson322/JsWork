76efc4d28ee3975a21cf72b7ad9c2db370d77cae
Nickson322 <77890953+Nickson322@users.noreply.github.com>
2022-10-16 22:59:36 +0300

    Add files via upload

---
+++ 16.js
@@ -0,0 +1,13 @@
+/** ЗАДАЧА 16 - Замена части строки
+ *
+ * 1. Создайте переменную и присвойте ей строку "Good Morning"
+ *
+ * 2. Замените "Morning" на "Evening" и присвойте результат новой переменной
+ *
+ * 3. Выведите значение второй переменной в консоль
+ */
+
+let greetings1 = "Good Morning";
+let greetings2 = greetings1.replace("Morning", "Evening");
+
+console.log(greetings2);
